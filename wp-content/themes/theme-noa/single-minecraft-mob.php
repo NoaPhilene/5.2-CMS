9c8101e259ffb3fd60cab86e7e3cfcf8008f5c10
NoaPhilene <philenevanmil@gmail.com>
2025-10-31 12:07:40 +0100

    bijna af

--- wp-content/themes/theme-noa/single-minecraft-mob.php
+++ wp-content/themes/theme-noa/single-minecraft-mob.php
@@ -23,8 +23,40 @@
                 <?php if (!empty($mob_drops['breeding'])) : ?>
                     <p><?php echo esc_html($mob_drops['breeding']); ?></p>
                 <?php endif; ?>
+                
+                <?php if ( !empty($mob_drops['on_dead']) ) : ?>
+                    <h3>Drops bij dood:</h3>
+                    <?php foreach ( $mob_drops['on_dead'] as $drop ) : ?>
+                        <p><?php echo esc_html($drop['dead_drops']); ?></p>
+                        <p><?php echo esc_html($drop['default_drop_chanche']); ?></p>
+                        <img src="<?php echo esc_url($drop['dead_drops_image']['url']); ?>">
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                
+                <?php if ( !empty($mob_drops['extra']) ) : ?>
+                    <h3>Extra dops using:</h3>
+                    <?php foreach ( $mob_drops['extra'] as $extra_drops ) : ?>
+                        <img src="<?php echo esc_url($extra_drops['using_image']['url']); ?>">
+                        <p><?php echo esc_html($extra_drops['using']); ?></p>
+                        <img src="<?php echo esc_url($extra_drops['receive_image']['url']); ?>">
+                        <p><?php echo esc_html($extra_drops['receive']); ?></p>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+
             <?php endif; ?>
 
+           <?php if( have_rows('health_and_feeding') ) : 
+
+                while ( have_rows('health_and_feeding') ): the_row();  ?>
+                        <p><?php echo get_sub_field('food'); ?></p>
+                        <?php if ( get_sub_field('heals') ) : ?>
+                            <p><?php echo get_sub_field('heals') ?></p>
+                        <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+            <p>tameable? <?php echo get_field('is_it_tameable'); ?><p>
+            <p>biomes where they spawns <?php echo get_field('spawn_biome'); ?><p>
 
             <?php 
         endwhile;
