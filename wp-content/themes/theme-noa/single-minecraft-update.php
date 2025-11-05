4dd8a1eebe47933b1da78812235788cef3031622
NoaPhilene <philenevanmil@gmail.com>
2025-11-06 00:10:57 +0100

    minecraft update extra

--- wp-content/themes/theme-noa/single-minecraft-update.php
+++ wp-content/themes/theme-noa/single-minecraft-update.php
@@ -1,43 +1,81 @@
 <?php get_header(); ?>
+
 <div id="AchtergrondMinecraftUpdate">
+
     <?php
     if ( have_posts() ) :
         while ( have_posts() ) : the_post();
+
+            // Banner, versie, datum
+            $banner = get_field('banner');
+            $versie_nummer = get_field('versie_nummer');
+            $gepubliceerde_datum = get_field('gepubliceerde_datum');
             ?>
 
-            <img src="<?php echo get_field('banner')['url'] ?>">
+            <?php if ( $banner && isset($banner['url']) ) : ?>
+                <img src="<?php echo esc_url($banner['url']); ?>" alt="Banner">
+            <?php endif; ?>
+
             <h1><?php the_title(); ?></h1>
 
             <div id="UpdateTitleBeschrijving"><?php the_content(); ?></div>
-            <p>Versie nummer: <?php echo get_field('versie_nummer'); ?><p>
-            <p>Gepubliceerde datum: <?php echo get_field('gepubliceerde_datum'); ?><p>
-            <?php if( have_rows('toegevoegde_functies') ) : 
 
-                while ( have_rows('toegevoegde_functies') ): the_row();  ?>
+            <?php if ( $versie_nummer ) : ?>
+                <p>Versie nummer: <?php echo esc_html($versie_nummer); ?></p>
+            <?php endif; ?>
 
-                    <h2><?php echo get_sub_field('functie_naam'); ?></h2>
+            <?php if ( $gepubliceerde_datum ) : ?>
+                <p>Gepubliceerde datum: <?php echo esc_html($gepubliceerde_datum); ?></p>
+            <?php endif; ?>
 
-                    <?php while ( have_rows('functie_uitleg') ): the_row(); ?>
-                        <p><?php echo get_sub_field('beschrijving'); ?> </p>
-                    <?php endwhile; ?>
-                    
-                        <?php if ( get_sub_field('functie_afbeelding_beschrijving')['afbeelding'] ) : ?>
-                            <img src="<?php echo get_sub_field('functie_afbeelding_beschrijving')['afbeelding']['url'] ?>">
-                            <?php if ( get_sub_field('functie_afbeelding_beschrijving')['beschrijving'] ) : ?>
-                                <p><?php echo get_sub_field('functie_afbeelding_beschrijving')['beschrijving'] ?></p>
+            <!-- Toegevoegde functies -->
+            <?php if ( have_rows('toegevoegde_functies') ) : ?>
+                <?php while ( have_rows('toegevoegde_functies') ) : the_row(); ?>
+
+                    <div class="functie-blok">
+                        <h2><?php echo esc_html( get_sub_field('functie_naam') ); ?></h2>
+
+                        <div id="MobBeschrijving">
+                            <ul>
+                                <?php while ( have_rows('functie_uitleg') ) : the_row(); ?>
+                                    <li>
+                                        <?php echo esc_html( get_sub_field('beschrijving') ); ?>
+
+                                        <?php if ( have_rows('extra_info') ) : ?>
+                                            <ul>
+                                                <?php while ( have_rows('extra_info') ) : the_row(); ?>
+                                                    <li><?php echo esc_html( get_sub_field('extra_extra') ); ?></li>
+                                                <?php endwhile; ?>
+                                            </ul>
+                                        <?php endif; ?>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+                        </div>
+
+                        <?php 
+                        // Functie afbeelding en beschrijving
+                        $afb_beschrijving = get_sub_field('functie_afbeelding_beschrijving');
+                        if ( is_array($afb_beschrijving) && !empty($afb_beschrijving['afbeelding']['url']) ) : ?>
+                            <img src="<?php echo esc_url($afb_beschrijving['afbeelding']['url']); ?>" alt="Functie afbeelding">
+                            <?php if ( !empty($afb_beschrijving['beschrijving']) ) : ?>
+                                <p><?php echo esc_html($afb_beschrijving['beschrijving']); ?></p>
                             <?php endif; ?>
                         <?php endif; ?>
 
+                    </div>
+
                 <?php endwhile; ?>
-            <?php else : ?> 
-                <p>Geen toegevoegde functies gevonden</p>
-            <?php endif; 
-            
-        endwhile;
+            <?php else : ?>
+                <p>Geen toegevoegde functies gevonden.</p>
+            <?php endif; ?>
+
+        <?php endwhile;
     else :
         echo '<p>Geen berichten gevonden.</p>';
     endif;
     ?>
-</div>
-<?php get_footer(); ?>
 
+</div>
+
+<?php get_footer(); ?>
